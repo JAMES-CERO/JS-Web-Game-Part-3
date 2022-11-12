eab1f2870131a9e2448d57d5af53180655a5a2df
James Aules <james_aules@icloud.com>
2022-11-12 13:22:28 -0500

    creating a function for movement

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-function newImage(url, left, bottom){
+function newImage(url){
     let image = document.createElement('img')
     image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
 }
 
-newImage('assets/green-character.gif', 100, 250)
+function move(image){
+    image.style.position = 'fixed'
+    
+    function moveToCoordinates(left, bottom){
+        image.style.left = left + 'px'
+        image.style.bottom = bottom + 'px'
+    }
+
+    return {
+        to: moveToCoordinates
+    }
+}
+
+
+move(newImage('assets/green-character.gif')).to(100, 250)
+
+
+//newImage('assets/green-character.gif', 100, 250)
 newImage('assets/tree.png', 200, 450)
 newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
+move(newImage('assets/pine-tree.png')).to(450, 350)
 newImage('assets/crate.png', 150, 350)
 newImage('assets/well.png', 500, 575)
 
@@ -26,6 +40,7 @@ function newItem(url, left, bottom){
     })
     return item
 }
+
 
 newItem('assets/sword.png', 500, 555)
 newItem('assets/shield.png', 165, 335)
@@ -49,3 +64,4 @@ function newInventory(){
 }
 
 const inventory = newInventory()
+
